9b3ef2a3b14f03c0d1957ee57f919d0676c43ef6
jdlee022 <jdlee022@gmail.com>
2017-03-07 14:00:38 -0800

    initial project setup

---
+++ assets/javascript/main.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyABAurtLfnJD7fa63YfFAXgrM3wl2u7A84",
+    authDomain: "project-surfnturf.firebaseapp.com",
+    databaseURL: "https://project-surfnturf.firebaseio.com",
+    storageBucket: "project-surfnturf.appspot.com",
+    messagingSenderId: "948418625999"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
