80265a5772e9d0b157059b04723ee532ec8d0372
jdlee022 <jdlee022@gmail.com>
2017-03-13 19:13:22 -0700

    accept tony's stuff

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,66 +9,66 @@ var places = [];
 var currentPlace;
 var currentPhotos = [];
 
-function getWeather(lat, lng){
+function getWeather(lat, lng) {
     var apikeyid = "eb3f27114445b4659aab2c8fd7a8fa5d";
-	var queryURL = "http://api.openweathermap.org/data/2.5/weather?"
-		+ "lat=" + lat 
-		+ "&lon=" + lng 
-		+ "&appid=" + apikeyid;
-	console.log(queryURL);
-	$.ajax({
-		url: queryURL,
-		method: 'GET' })
-		.done(function(response){
-			console.log(response);
-		});
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +
+        "lat=" + lat +
+        "&lon=" + lng +
+        "&appid=" + apikeyid;
+    console.log(queryURL);
+    $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        .done(function (response) {
+            console.log(response);
+        });
 }
 
 /** Call this to get current location and store lat & lng in currentLot */
 function initMap() {
-   
+
     //if browser supports current location then store it in currentLot, else get from user input
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) { 
-            currentLot = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-                switch: true
-            };
-           
-            console.log("initMap Latitude: " + currentLot.lat + ". Longitude: " + currentLot.lng);
-            //Check if the API has returned the user's current location. If so the if statement would run
-            if(currentLot.switch){
-                initializePlaces(currentLot.lat, currentLot.lng);
-                getWeather(currentLot.lat, currentLot.lng);
 
+    navigator.geolocation.getCurrentPosition(function (position) {
+        currentLot = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+            switch: true
+        };
 
-            }
-        }, function(){console.log("error"); }, {timeout:5000});
-    } else {
-        //TODO: if unable to get current location then prompt user to enter one manually
-        prompt('Please input your location');  
-    }
+        console.log("initMap Latitude: " + currentLot.lat + ". Longitude: " + currentLot.lng);
+        //Check if the API has returned the user's current location. If so the if statement would run
+        if (currentLot.switch) {
+            initializePlaces(currentLot.lat, currentLot.lng);
+            getWeather(currentLot.lat, currentLot.lng);
+        }
+    }, function () {
+        //TODO: if geolocation doesnt work then prompt user to enter a location
+        console.log("error");
+    }, {
+        timeout: 5000
+    });
 }
 
 /** gets places based on given lat & lng */
 function initializePlaces(lat, lng) {
-        var pyrmont = new google.maps.LatLng(lat, lng);
+    var pyrmont = new google.maps.LatLng(lat, lng);
 
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: pyrmont,
-            zoom: 15
-        });
-        //set data that determines what is returned
-        var textRequest = {
-            location: pyrmont,
-            radius: '500',
-            query: 'hiking'
-        };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
+    });
+    //set data that determines what is returned
+    var textRequest = {
+        location: pyrmont,
+        radius: '500',
+        query: 'hiking'
+    };
 
-        service = new google.maps.places.PlacesService(map);
-        //GET the data (see searchCallBack function)
-        service.textSearch(textRequest, searchCallback);
+    service = new google.maps.places.PlacesService(map);
+    //GET the data (see searchCallBack function)
+    service.textSearch(textRequest, searchCallback);
 }
 
 /** Handles the data that is returned by google places */
@@ -130,6 +130,7 @@ function getPhotos(currentPlace) {
 /** When the user clicks a button to load a new place update the array of current pictures */
 $("#newPlace").on("click", function () {
     getPhotos(places[counter]);
+    getWeather(places[counter].lat, places[counter].lng);
     counter++;
 });
 
