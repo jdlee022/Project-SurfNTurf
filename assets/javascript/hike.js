186cf445e54d12f8d2ae0e3e7853d388a34aeff4
Sean Naylor <Sean Naylor>
2017-03-15 20:19:14 -0700

    finishin up hike.html/css

---
+++ assets/javascript/hike.js
@@ -0,0 +1,9 @@
+//hide the favorites list
+$( "#favList" ).click(function() {
+  $( "#favoritesWrapper" ).toggle( "slow", function() {
+    
+  });
+});
+
+//fluidly resize spot-name text
+//$("#name-spot").fitText(2.5);
